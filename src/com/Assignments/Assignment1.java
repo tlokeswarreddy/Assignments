0e45a17bcc6e4a1f6c1336218dbe59d640048fa4
tlokeswarreddy <lokeshwarareddy61@gmail.com>
2025-06-17 20:29:04 +0530

    empty

--- src/com/Assignments/Assignment1.java
+++ src/com/Assignments/Assignment1.java
@@ -3,8 +3,7 @@ package com.Assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		System.out.println("Lokeswara Reddy");
-		System.out.println("AAdvika Reddy");
+		
 
 	}
 
