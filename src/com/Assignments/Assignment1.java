78c7d38a74f19b360ee889dabf4964f7f99b88d7
tlokeswarreddy <lokeshwarareddy61@gmail.com>
2025-06-17 20:56:58 +0530

    data types

--- src/com/Assignments/Assignment1.java
+++ src/com/Assignments/Assignment1.java
@@ -3,8 +3,103 @@ package com.Assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
+		//1. Temperature of a city in degrees Celsius: 25.5
+		
+		float d=25.5f;
+		
+		//2. Whether a customer has placed an order: true or false
+		
+		boolean b=true;
+		
+		//3. Person's phone number: "123-456-7890"
+		
+		String phoneNumber="9066981102";
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		
+		float account=1000.50f;
+		
+		//5. Person's email address: "john.doe@example.com"
+		
+		String email="john.doe@example.com";
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		
+		double lat=37.7749;
+		double lon=122.4194;
+		
+		//7. Person's marital status: true or false
+		
+		boolean marriageStatus=true;
+		
+		//8. Person's occupation: "Software Engineer"
+		
+		String occupation = "Software Engineer";
+		
+		//9. Person's favourite colour: "Blue"
+		
+		String colour="Blue";
+		
+		//10.Current year: 2023
+		
+		int year=2023;
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		
+		String followers="1,000,000";
+		
+		//12.Rating of a movie: 7.5
+		
+		float movierateing = 7.5f;
+		
+		//13.Person's blood type: 'A'
+		
+		char bloodtype='A';
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		
+		String Title="To Kill a Mockingbird";
+		
+		//15.Number of employees in a company: 500
+		
+		int employees =500;
+		
+		//16.Time of an event: 2:30 PM
+		
+		String time="2:30 PM";
+		
+		//17.Name of a country: "United States"
+		
+		String country="United States";
+		
+		//18.Person's eye color: "Brown"
+		
+		String eyecolor="Brown";
+		//19.Person's birthplace: "New York City"
+		
+		String birthplace = "New York City";
+		
+		//20.Distance between two cities: 200.5
+		
+		float cities=200.5f;
+		
+		System.out.println("Print all the data type values\n"
+				+d +"\n"+b
+				
+				);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 
 	}
+	
+	
 
 }
